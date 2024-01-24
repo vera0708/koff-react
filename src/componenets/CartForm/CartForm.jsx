cc1cc5f93ab1d7bb0060952060e0d53a85c7ff5a
Vera Deeva <verasdee@gmail.com>
2024-01-24 17:40:13 +0700

    add CartPlace and CartForm

--- src/componenets/CartForm/CartForm.jsx
+++ src/componenets/CartForm/CartForm.jsx
@@ -1,5 +1,65 @@
 import s from './CartForm.module.scss';
 
 export const CartForm = () => (
-    <div className={s.cartform}>CartForm</div>
+    <form className={s.form}>
+        <h3 className={s.subtitle}>Данные для доставки</h3>
+        <fieldset className={s.fieldsetInput}>
+            <input className={s.input}
+                type='text'
+                name='name'
+                placeholder='Фамилия Имя Отчество' />
+            <input className={s.input}
+                type='text'
+                name='phone'
+                placeholder='Телефон' />
+            <input className={s.input}
+                type='email'
+                name='email'
+                placeholder='E-mail' />
+            <input className={s.input}
+                type='text'
+                name='address'
+                placeholder='Адрес доставки' />
+            <textarea className={s.textarea}
+                name='comments'
+                placeholder='Комментарий к заказу'
+            ></textarea>
+        </fieldset>
+
+        <fieldset className={s.fieldsetRadio} >
+            <legend className={s.legend}>Доставка</legend>
+            <label className={s.radio}>
+                <input className={s.radioInput}
+                    type='radio'
+                    name='deliveryType'
+                    value='delivery' />
+                Доставка
+            </label>
+            <label className={s.radio}>
+                <input className={s.radioInput}
+                    type='radio'
+                    name='deliveryType'
+                    value='pickup' />
+                Самовывоз
+            </label>
+        </fieldset>
+
+        <fieldset className={s.fieldsetRadio} >
+            <legend className={s.legend}>Оплата</legend>
+            <label className={s.radio}>
+                <input className={s.radioInput}
+                    type='radio'
+                    name='paymentType'
+                    value='card' />
+                Картой при получении
+            </label>
+            <label className={s.radio}>
+                <input className={s.radioInput}
+                    type='radio'
+                    name='paymentType'
+                    value='cash' />
+                Наличными при получении
+            </label>
+        </fieldset>
+    </form>
 )
--- src/componenets/CartPlace/CartPlace.jsx
+++ src/componenets/CartPlace/CartPlace.jsx
@@ -1,5 +1,13 @@
 import s from './CartPlace.module.scss';
 
 export const CartPlace = () => (
-    <div className={s.cartplace}>CartPlace</div>
+    <div className={s.place}>
+        <h3 className={s.subtitle}>Оформление</h3>
+        <div className={s.placeInfo}>
+            <p >4 товара на сумму</p>
+            <p >13&nbsp;700&nbsp;₽</p>
+        </div>
+        <p className={s.placeDelivery}>Доставка 0&nbsp;₽</p>
+        <button className={s.placeBtn}>Оформить заказ</button>
+    </div>
 )
